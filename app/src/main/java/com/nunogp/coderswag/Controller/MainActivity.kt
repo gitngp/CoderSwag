3fe6e08b0af8fca5496a029dc2939e6e624a26b3
gitngp <nunogouveiap@gmail.com>
2020-06-24 19:02:25 +0100

    simple listview

--- app/src/main/java/com/nunogp/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/nunogp/coderswag/Controller/MainActivity.kt
@@ -2,11 +2,25 @@ package com.nunogp.coderswag.Controller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.nunogp.coderswag.Model.Category
 import com.nunogp.coderswag.R
+import com.nunogp.coderswag.Services.DataService
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter : ArrayAdapter<Category>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //adapter sends data to listview
+        adapter = ArrayAdapter(this,
+        android.R.layout.simple_list_item_1,
+        DataService.categories)
+        //id
+        category_listView.adapter = adapter
     }
 }
--- app/src/main/java/com/nunogp/coderswag/Model/Category.kt
+++ app/src/main/java/com/nunogp/coderswag/Model/Category.kt
@@ -1,4 +1,8 @@
 package com.nunogp.coderswag.Model
 
-class Category(val title: String, val image: String)
+class Category(val title: String, val image: String){
+    //show title in listview
+    override fun toString(): String {
+        return title
+    }
 }
