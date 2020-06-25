c43d03e3424bb95f55a25fddbdd9c8f7318a4cf2
gitngp <nunogouveiap@gmail.com>
2020-06-25 15:26:27 +0100

    create a costume listview adapter

---
+++ app/src/main/java/com/nunogp/coderswag/Adapters/CategoryAdapter.kt
@@ -0,0 +1,47 @@
+package com.nunogp.coderswag.Adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.nunogp.coderswag.Model.Category
+import com.nunogp.coderswag.R
+
+class CategoryAdapter(context: Context, categories: List<Category>):BaseAdapter() {
+
+    val context = context
+    val categories = categories
+
+    //override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
+     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+       //to change body of created functions use | settings | File Template
+        val categoryView : View
+        categoryView = LayoutInflater.from(context).inflate(R.layout.category_list_item, null)
+        val categoryImage : ImageView = categoryView.findViewById(R.id.categoryImage)
+        //textfield ID categoryName
+        val categoryName : TextView = categoryView.findViewById(R.id.categoryName)
+        val category =  categories[position]
+        //convert image name into id resource
+        val resourceId =  context.resources.getIdentifier(category.image, "drawable", context.packageName)
+        //imageId categoryName
+        categoryImage.setImageResource(resourceId)
+        //println(resourceId)
+        categoryName.text = category.title
+        return categoryView
+    }
+
+    override fun getItem(position: Int): Any {
+       return categories[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return 0
+    }
+
+    override fun getCount(): Int {
+        return categories.count()
+    }
+}
--- app/src/main/java/com/nunogp/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/nunogp/coderswag/Controller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.nunogp.coderswag.Controller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import com.nunogp.coderswag.Adapters.CategoryAdapter
 import com.nunogp.coderswag.Model.Category
 import com.nunogp.coderswag.R
 import com.nunogp.coderswag.Services.DataService
@@ -10,16 +11,17 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter : ArrayAdapter<Category>
+    lateinit var adapter : CategoryAdapter//ArrayAdapter<Category>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         //adapter sends data to listview
-        adapter = ArrayAdapter(this,
-        android.R.layout.simple_list_item_1,
-        DataService.categories)
+        //adapter = ArrayAdapter(this,
+        //android.R.layout.simple_list_item_1,
+        //DataService.categories)
+        adapter = CategoryAdapter(this, DataService.categories)
         //id
         category_listView.adapter = adapter
     }
